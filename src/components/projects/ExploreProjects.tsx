44633d1408b63be9b798f263cc1e2f0ec83a254a
rogerjeasy <rogerjeasy@gmail.com>
2025-01-10 20:19:24 +0100

    improved the why project component

--- src/components/projects/ExploreProjects.tsx
+++ src/components/projects/ExploreProjects.tsx
@@ -1,19 +1,28 @@
 "use client"
-
-
 import ProjectHeaderSection from "./ProjectHeaderSection"
 import ProjectSearchFilterSection from "./ProjectSearchFilterSection";
-import WhyExploreProjects from "./WhyExploreProjects";
 import PublicProjectsPage from "./PubicProjects";
+import { Button } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 
 const ExploreProjects = () => {
+  const router = useRouter();
   return (
     <div>
       <ProjectHeaderSection />
       <ProjectSearchFilterSection />
       <PublicProjectsPage />
-      <WhyExploreProjects />
+      <div className="flex justify-center mt-8 mb-8">
+        <Button
+          size="lg"
+          color="success"
+          onClick={() => router.push("/projects/why-projects")}
+        >
+          Why Projects ?
+        </Button>
+      </div>
     </div>
   );
 };
+
 export default ExploreProjects;
